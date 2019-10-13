5bb7d163ff96314bb4a1a3215adcaf408a3059c5
Volt <sonianadashkevich@gmail.com>
2019-10-13 20:03:00 +0200

    zadanie 1 method changed, dialog moved to main zadanie 2 fields instead of getters, dialog moved to main zadanie 3 corrected

---
+++ src/zadanie3/Test.java
@@ -0,0 +1,13 @@
+package zadanie3;
+
+public class Test {
+    public static void main(String[] args) {
+        Product product1 = new Product("Mleko", "Produkty zywnosciowe", "Mleko 1l 0,5% tluszczu",
+                1.80);
+        Product product2 = new Product("Mikser", "Maly sprzet kuchenny",
+                "Mikser 1300W 2 komplety zmiennych koncowek", 85);
+        BruttoCalc calc1 = new BruttoCalc();
+        System.out.println(calc1.brutto(product1));
+        System.out.println(calc1.brutto(product2));
+    }
+}
